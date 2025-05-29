9fae108ae775dba845ffbe827d5125047b2ab066
Faris Al-Sahili <farisasahili@gmail.com>
2025-05-29 19:04:33 +0300

    Remove backend changes from design-ui branch

--- apps/jobs/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import JobPost
-
-@admin.register(JobPost)
-class JobPostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'company', 'location', 'employment_type', 'experience_level', 'posted_at')
-    list_filter = ('employment_type', 'experience_level', 'location', 'posted_at')
-    search_fields = ('title', 'description', 'location', 'company__name')
