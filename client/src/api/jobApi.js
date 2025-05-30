51e4e962ea3ba739bcea3c8a29ab6aa726289412
Faris Al-Sahili <farisasahili@gmail.com>
2025-05-30 20:50:44 +0300

    Fix base url v2

--- client/src/api/jobApi.js
+++ client/src/api/jobApi.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:8000/";
+const BASE_URL = "https://jobboard-system.onrender.com/api";
 
 const getToken = () => localStorage.getItem("accessToken");
 
