07ad246bc0d430fe20bb6dff6c8af858b42603dd
Faris Al-Sahili <farisasahili@gmail.com>
2025-05-30 20:42:20 +0300

    Fix base url

--- client/src/api/jobApi.js
+++ client/src/api/jobApi.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:8000/api";
+const BASE_URL = "http://localhost:8000/";
 
 const getToken = () => localStorage.getItem("accessToken");
 
